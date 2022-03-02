6c7828021776fea36f1f06815458b213cd0cb1a3
Bhavna Shende <bhavanashende661@gmail.com>
2022-03-02 00:47:58 -0500

    Added: Added Twitter API Testing

--- src/test/java/com/bridgelabz/restassuredapi/TestAPI.java
+++ src/test/java/com/bridgelabz/restassuredapi/TestAPI.java
@@ -2,6 +2,7 @@ package com.bridgelabz.restassuredapi;
 
 import io.restassured.RestAssured;
 import io.restassured.internal.path.json.mapping.JsonObjectDeserializer;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
@@ -10,8 +11,8 @@ import org.testng.annotations.Test;
 public class TestAPI {
 
     @Test
-    public void getTest(){
-        Response response= RestAssured.get("http://localhost:3000/posts");
+    public void getTest() {
+        Response response = RestAssured.get(" http://localhost:3000/posts");
         System.out.println("Response code: " + response.getStatusCode());
         response.prettyPrint();
     }
@@ -20,8 +21,8 @@ public class TestAPI {
         RequestSpecification requestSpecification= RestAssured.given();
         requestSpecification.header("Content-Type", "application/json");
         JSONObject jsonObject=new JSONObject();
-        jsonObject.put("id", 10);
-        jsonObject.put("title", "Bhavna");
+        jsonObject.put("id", 11);
+        jsonObject.put("title", "Billi");
         jsonObject.put("author", "TestAPI");
         requestSpecification.body(jsonObject.toJSONString());
         Response response = requestSpecification.post("http://localhost:3000/posts");
@@ -34,7 +35,7 @@ public class TestAPI {
         requestSpecification.header("Content-Type", "application/json");
         JSONObject jsonObject = new JSONObject();
         //jsonObject.put("id", 3);
-        jsonObject.put("title", "Bhanu");
+        jsonObject.put("title", "PutBhanu");
         jsonObject.put("author", "RestAPI");
         requestSpecification.body(jsonObject.toJSONString());
         Response response = requestSpecification.put("http://localhost:3000/posts/10");
@@ -45,7 +46,7 @@ public class TestAPI {
     public void deleteTest() {
         RequestSpecification requestSpecification = RestAssured.given();
         requestSpecification.header("Content-Type", "application/json");
-        Response response = requestSpecification.delete("http://localhost:3000/posts/10");
+        Response response = requestSpecification.delete("http://localhost:3000/posts/11");
         System.out.println("responseCode: " + response.getStatusCode());
         response.prettyPrint();
 
--- src/test/java/com/bridgelabz/restassuredapi/TweetTesting.java
+++ src/test/java/com/bridgelabz/restassuredapi/TweetTesting.java
@@ -1,2 +1,61 @@
-package com.bridgelabz.restassuredapi;public class TweetTesting {
+package com.bridgelabz.restassuredapi;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class TweetTesting {
+
+
+    @Test
+    public void postTest() {
+        Response response = RestAssured.given()
+                .auth()
+                .oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
+                        "qkf8Rj3kggsjkvc2mF5sBLvE8k93ZL9Q74nPzKujFwJ8QAofoV",
+                        "4003503614-NqJnAeZ2s6oHIPEWsNJnKDoKB4TtIPD2oOFUSuW",
+                        "CsW3W4jEd4OarUqbqaJSSVzy808HEAB5uyGyZDA9QrLZR")
+                .post("https://api.twitter.com/1.1/statuses/update.json?status= Bhanu");
+
+        System.out.println("status code: " + response.getStatusCode());
+        System.out.println("time taken: " + response.getTime());
+        response.prettyPrint();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        String id_str = jsonPath.getString("id_str");
+        System.out.println("***************");
+        System.out.println(id_str);
+
+    }
+
+    @Test
+    public void retweet() {
+        Response response = RestAssured.given().auth().
+                oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
+                        "qkf8Rj3kggsjkvc2mF5sBLvE8k93ZL9Q74nPzKujFwJ8QAofoV",
+                        "4003503614-NqJnAeZ2s6oHIPEWsNJnKDoKB4TtIPD2oOFUSuW",
+                        "CsW3W4jEd4OarUqbqaJSSVzy808HEAB5uyGyZDA9QrLZR")
+                .post("https://api.twitter.com/1.1/statuses/retweet/1497088794108989440.json");
+
+        System.out.println("Response code: " + response.getStatusCode());
+        System.out.println("Response jason Body: " + response.jsonPath().prettyPrint());
+
+    }
+
+    @Test
+    public void delete() {
+
+        Response response = RestAssured.given().auth().
+                oauth("TTQQO4y0DJGZLBU2LcU1VJECQ",
+                        "qkf8Rj3kggsjkvc2mF5sBLvE8k93ZL9Q74nPzKujFwJ8QAofoV",
+                        "4003503614-NqJnAeZ2s6oHIPEWsNJnKDoKB4TtIPD2oOFUSuW",
+                        "CsW3W4jEd4OarUqbqaJSSVzy808HEAB5uyGyZDA9QrLZR")
+                .delete("https://api.twitter.com/2/tweets/1497088794108989440");
+
+        System.out.println("Response code: " + response.getStatusCode());
+        response.prettyPrint();
+    }
 }
+
